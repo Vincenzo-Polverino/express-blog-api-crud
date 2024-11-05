50cabd4f019267df1575b6f11efb71aad4c5ce06
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-05 17:19:33 +0100

    add markup

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,25 +1,35 @@
 const posts = require('../database/db.js')
 
 const index = (req, res) => {
-    res.json({ 
-        data: posts, 
-        count: posts.length 
-    })
-  }
+    let markup = `
+        <ul>
+            ${posts.map(post => `
+                <li>
+                    <h2>${post.title}</h2>
+                    <p>${post.content}</p>
+                    <img src="${post.image}" alt="${post.title}" style="width:200px;height:auto;">
+                </li>
+            `).join('')}
+        </ul>
+    `;
+    res.send(markup);
+};
 
-  const show = (req, res) => {
-  
+
+
+const show = (req, res) => {
+
     const post = posts.find((post) => post.slug.toLowerCase() === req.params.slug)
     if (!post) {
-      return res.status(404).json({ error: "Nessun post trovato" })
+        return res.status(404).json({ error: "Nessun post trovato" })
     }
     return res.status(200).json({ data: post })
-  }
+}
 
 
 
 
-  module.exports = {
+module.exports = {
     index,
     show
-  }
+}
