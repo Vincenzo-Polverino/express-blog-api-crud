8d82b4c216744f75668f8ea4e65da4dd230526cd
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-05 17:02:46 +0100

    fix postController path

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const postController = require('./controllers/postController');
+const postController = require('../controllers/postController');
 
 router.get('/', postController.index);
 router.get('/:slug', postController.show)
