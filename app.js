3281ef4b0f6d4567caed63668d2f5d4383df7358
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-05 16:11:13 +0100

    add second endpoint

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ app.use(express.static('/public'))
 
 app.listen(3000, () => {
     console.log("Server started on port 3000")
-  });
+});
 
 
-  app.get('/posts', (req, res) => {
-    res.json({ data: posts, count: posts.length })
+app.get('/posts', (req, res) => {
+    res.json(
+        {
+            data: posts,
+            count: posts.length
+        })
+});
+
+app.get('/posts/:slug', (req, res) => {
+
+  
+    const post = posts.find((post) => post.slug.toLowerCase() === req.params.slug)
+    if (!post) {
+      return res.status(404).json({ error: "Nessun post trovato" })
+    }
+    return res.status(200).json({ data: post })
   });
