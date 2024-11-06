20e8e920cde520c2ffa311de34626b819af8b481
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-06 15:50:30 +0100

    fix store router

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const postController = require('../controllers/postController');
 
 router.get('/', postController.index);
-router.post('/posts', postController.store)
+router.post('/', postController.store)
 router.get('/:slug', postController.show)
 router.put('/:slug', postController.update)
 
