20c8e7347f0ba754b592d1a1f3e657dc627f2629
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-07 14:46:21 +0100

    add error heandler

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const postRoutes = require('./routes/post')
 const not_found = require('./middlewares/not_found.js')
 const logger = require('./middlewares/logger.js')
 
+app.use('/posts', (req, res, next) => {
+    throw new Error("Hai rotto tutto!");
+  }); 
+
 app.use(express.json());
 app.use(express.static('/public'))
 app.use('/posts', logger)
@@ -13,3 +17,12 @@ app.use(not_found)
 app.listen(3000, () => {
     console.log("Server started on port 3000")
 });
+
+app.use((err, req, res, next) => {
+    console.log("Error: ", err.message);
+    console.error(err.stack);
+    res.status(500).send({
+      message: "Qualcosa è andato storto!",
+      error: err.message
+    })
+  });
